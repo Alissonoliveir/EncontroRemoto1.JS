ade70f7d265571748a778ac4def584c3150e6464
Alissonoliveir <102183299+Alissonoliveir@users.noreply.github.com>
2022-04-18 21:52:14 -0300

    situação de aprendizagem

---
+++ SA1.js
@@ -0,0 +1,21 @@
+let listaPecas = ["computador" , "mouse" , "teclado"];
+let nomePeca = "pc";
+let pesoPeca = 350;
+
+
+if(listaPecas.length >=10 ){
+console.log("limite de 10 peças atingido");
+}else{
+    if(pesoPeca >= 100){
+        console.log("a peça possui mais de 100g e pode ser cadastrada");
+        if(nomePeca.length < 3){
+            console.log("erro nome da peça tem que ter mais de 3 caracteres");
+        }
+
+
+}
+
+
+
+
+}
